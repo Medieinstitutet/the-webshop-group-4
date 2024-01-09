8a68513e1cec7186c232770c355276b63b9698d4
Erik Lindner <erik.lindner@workmail.com>
2024-01-09 13:18:19 +0100

    checkoutClick

--- src/ts/services/checkout.ts
+++ src/ts/services/checkout.ts
@@ -4,23 +4,25 @@ let cart: Product[] = [];
 
 //declartion
 const displayCheckout = () => {
-  const checkoutListHTML: Element | null = document.getElementById("checkoutList");
-  const checkoutTotalHTML: Element | null = document.getElementById("checkoutTotal");
+  const checkoutListHTML: Element | null =
+    document.getElementById("checkoutList");
+  const checkoutTotalHTML: Element | null =
+    document.getElementById("checkoutTotal");
 
-  //clean out everything to 0 
+  //clean out everything to 0
   if (checkoutListHTML && checkoutTotalHTML) {
     checkoutListHTML.innerHTML = "";
     let total = 0;
-  
 
     // use filter to find every quantiy which is bigger than 0
-    const filteredCart = cart.filter(function(cartProduct) {
+    const filteredCart = cart.filter(function (cartProduct) {
       return cartProduct.quantity > 0;
     });
 
     // Use forEach to reach every cart items and create new div, unit to show in the page. use appendChild to make sure every childran come with.
     filteredCart.forEach((cartProduct) => {
-    let checkoutProductElement: HTMLDivElement = document.createElement("div");
+      let checkoutProductElement: HTMLDivElement =
+        document.createElement("div");
       checkoutProductElement.classList.add("unit");
       checkoutProductElement.innerHTML = `
         <div class="image">
@@ -28,7 +30,9 @@ const displayCheckout = () => {
         </div>
         <div class="info">
           <div class="name">${cartProduct.productName}</div>
-          <div class="totalPrice">${cartProduct.price * cartProduct.quantity}kr</div>
+          <div class="totalPrice">${
+            cartProduct.price * cartProduct.quantity
+          }kr</div>
           <div class="quantity">
            
             <span>${cartProduct.quantity}</span>
@@ -36,22 +40,29 @@ const displayCheckout = () => {
           </div>
         </div>
       `;
-    checkoutListHTML.appendChild(checkoutProductElement);
+      checkoutListHTML.appendChild(checkoutProductElement);
 
       // Update the total price
-      total = total+cartProduct.price * cartProduct.quantity;
+      total = total + cartProduct.price * cartProduct.quantity;
     });
 
     //total price in the checkout page
     checkoutTotalHTML.textContent = `Total: ${total}kr`;
-
   }
-    };
-    
+};
 
 // Load the cart from local storage
 const savedCart = localStorage.getItem("cart");
 cart = savedCart ? JSON.parse(savedCart) : [];
 
-// Display 
+function checkoutClick() {
+  const checkoutButton = document.getElementById("checkoutForm");
+  checkoutButton?.addEventListener("submit", (event) => {
+  event.preventDefault();
+   alert("Din beställning har skickats!");
+  });
+}
+
+// Display
 displayCheckout();
+checkoutClick();
